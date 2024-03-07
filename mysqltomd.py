fb4cc877038589e9a289f1f5427a0fd1a58490fb
Scobiform <datenkrake@scobiform.com>
2024-03-07 23:24:26 +0100

    filter-out colons

--- mysqltomd.py
+++ mysqltomd.py
@@ -1,5 +1,6 @@
 import mysql.connector
 import os
+import re
 
 config = {
     'user': 'root',
@@ -8,6 +9,10 @@ config = {
     'database': 'scobiform',
     'raise_on_warnings': True
 }
+
+# Function to remove colons using regex
+def remove_colons(text):
+    return re.sub(r":", "", text)
 
 try:
     cnx = mysql.connector.connect(**config)
@@ -22,6 +27,9 @@ try:
 
     for (id, title, body, slug, created_at) in cursor:
         filename = f"{output_dir}/{slug}.md"
+        # Removing colons from the strings
+        slug = remove_colons(slug)
+        title = remove_colons(title)
         with open(filename, 'w', encoding='utf-8') as f:
             f.write(f"---\ntitle: {title}\ndate: {created_at}\ndescription: {title}\ntag: \nauthor: scobiform\n---\n{body}")
 
